c7bb29b61b1a5f4e20598b7cb3a44116cae0d1b9
Farah <abdifitah.a.f@gmail.com>
2024-06-09 12:07:14 +0100

    setup structure of game fuctions

---
+++ assets/js/scripts.js
@@ -0,0 +1,33 @@
+function runGame(){
+
+}
+
+function checkanswer(){
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer()
+{
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function subtractQuestion() {
+
+}
+
+
+function multiplyQuestion(){
+
+}
